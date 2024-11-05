2f9198062a8f680ef67620438dbb7baba93009e2
evolve <elivanoa5@gmail.com>
2024-11-05 15:03:37 +0100

    created main.cs

---
+++ cs/main.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Yams {
+
+  struct joueur
+  {
+    int id;               // id du joueur, 1 ou 2 etant donnée que le nombre max de jouer est 2
+    string pseudo;        // pseudo du joueur
+    int[,] dices;         
+    int[,] scoreRounds;
+    int[] challenges;
+    int scoreTotal;
+  }
+
+  static void Main() {
+  
+  }
+}
