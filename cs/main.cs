42f5778a0f6f00433e1d3a6e8ead285b7333ea97
evolve <elivanoa5@gmail.com>
2024-11-05 15:18:53 +0100

    added comments for joueur structure

--- cs/main.cs
+++ cs/main.cs
@@ -6,10 +6,10 @@ class Yams {
   {
     int id;               // id du joueur, 1 ou 2 etant donnée que le nombre max de jouer est 2
     string pseudo;        // pseudo du joueur
-    int[,] dices;         
-    int[,] scoreRounds;
-    int[] challenges;
-    int scoreTotal;
+    int[,] dices;         // on va stocker tous les des finaux de chaque rounds
+    int[] challenges;     // les challenges encore utilisable par le joueur
+    int[,] scoreRounds;   // les scores de chaque score
+    int scoreTotal;       // le score final
   }
 
   static void Main() {
