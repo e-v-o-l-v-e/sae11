f6614e654c336205b8bdcd92f892f1a04ab2b61f
evolve <elivanoa5@gmail.com>
2024-11-10 15:48:38 +0100

    avancé dans la fonction principale et demarré tour

--- cs/main.cs
+++ cs/main.cs
@@ -2,15 +2,17 @@ using System;
 
 class Yams {
 
-  public struct createPlayer
+  static struct createPlayer
   {
     int id;               // id du joueur, 1 ou 2 etant donnée que le nombre max de jouer est 2
     string pseudo;        // pseudo du joueur
     int[,] dices;         // on va stocker tous les des finaux de chaque rounds
-    int[] challenges;     // les challenges encore utilisable par le joueur
+    int[,] challenges;     // les challenges encore utilisable par le joueur
     int[,] scoreRounds;   // les scores de chaque score
     int scoreTotal;       // le score final
   }
+
+  static challenges 
 
   static void Main() {
   
@@ -21,11 +23,17 @@ class Yams {
     player2.id=2;         // initialisation de son id a 1
 
     Console.Write("Entrez le pseudo du premier joueur : ");
-    player1.pseudo = string.Parse (Console.Read ());
+    player1.pseudo = string.Parse(Console.Read ());
     
     Console.Write("Entrez le pseudo du second joueur : ");
-    player2.pseudo = string.Parse (Console.Read ());
+    player2.pseudo = string.Parse(Console.Read ());
     
+    // tours de jeu
+    for ( int i = 1 ; i <= 13 ) {
+      tour(player1)
+    }
   }
+
+  static void tour (ref createPlayer currentPlayer)
 }
 
