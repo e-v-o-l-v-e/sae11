53c2f686b38c15e6e52b2c8e91dce80de01e76fb
AmbreRoise <el0r4.fll@gmail.com>
2024-11-14 14:01:58 +0100

    Update fonction_challengeRestant.cs

--- fonction_challengeRestant.cs
+++ fonction_challengeRestant.cs
@@ -1,7 +1,7 @@
 public static void challengeRestant(ref CreatePlayer player, string[] challenges){
   for(int i=0; i<5; i++){
     if(player.challenges[i] != 0){
-      Console.WriteLine(challenges[player.challenges -1]);
+      Console.WriteLine(challenges[i] + " - " + challenges[player.challenges -1]);
     }
   }
 }
