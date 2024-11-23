a78d9d28d1ab43245db58fd5407f1d79cf235ac2
AmbreRoise <el0r4.fll@gmail.com>
2024-11-23 17:55:50 +0100

    Update script.js

--- html/script.js
+++ html/script.js
@@ -1,10 +1,23 @@
-let nomFichier;
 const form = document.querySelector('form');
 
 form.addEventListener("submit", (event) => {
-    // On récupère le nom de la partie
-    nomFichier = document.getElementById("nom_partie").value;
+    // On récupère le nom de la partie et on affiche sa valeur
+    let nomFichier = document.getElementById("nom_partie").value;
+    console.log("Nom de la partie:", nomFichier);
+    
+    // Vérifier quel bouton radio est sélectionné
+    let affichage = document.querySelector('input[name="partie"]');
+    let choix = affichage.value;
+    
+    /*    Si on veut dire qu'on a oublié de sélectionner un choix
+    if (affichage) {
+        const choix = affichage.value;
+    } else {
+        alert("Aucun choix de partie n'a été sélectionné.");
+    }
+    */
 });
+
 
 
 fetch('yams.iutrs.unistra.fr:3000/api/games/$(nomFichier)')
