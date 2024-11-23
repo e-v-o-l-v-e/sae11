326c93bd96fbecc161803d171220399b92ef67c9
AmbreRoise <el0r4.fll@gmail.com>
2024-11-23 17:31:18 +0100

    Update script.js

--- html/script.js
+++ html/script.js
@@ -6,13 +6,12 @@ form.addEventListener("submit", (event) => {
     nomFichier = document.getElementById("nom_partie").value;
 });
 
-/*
+
 fetch('yams.iutrs.unistra.fr:3000/api/games/$(nomFichier)')
     .then(response => response.json())
     .then(data => {
         
         
-    }
+    })
     .catch(error => console.error('Erreur de chargement du fichier', error));
     
-*/
