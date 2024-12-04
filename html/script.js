3c9d21e27044c261b650e280eb549d006d6a2630
AmbreRoise <el0r4.fll@gmail.com>
2024-12-04 21:44:09 +0100

    Update script.js

--- html/script.js
+++ html/script.js
@@ -2,47 +2,230 @@ const form = document.querySelector('form');
 let nomFichier;
 let affichage;
 let choix;
+let codeHTML;
 
-form.addEventListener("submit", (event) => {
-    // On récupère le nom de la partie et on affiche sa valeur
-    nomFichier = document.getElementById("nom_partie").value;
+document.getElementById('formulaire').addEventListener('submit', nomFichier_choixAffichage);
+const contenantParametres = document.getElementById('contenuParametres');
+const contenantJoueurs = document.getElementById('contenuJoueurs');
+const contenantTours = document.getElementById('contenuTours');
+const contenantScoreFinal = document.getElementById('contenuScoreFinal');
+const contenantBoutonPrecedent = document.getElementById('boutonPrecedent');
+const contenantBouton = document.getElementById('boutonTour');
 
+
+function nomFichier_choixAffichage(event){
+    event.preventDefault();
+    nomFichier = document.getElementById("nomPartie").value;
     
-    // Vérifier quel bouton radio est sélectionné
     affichage = document.querySelector('input[name="partie"]:checked');
-    choix = affichage.value;
     
-    /*    Si on veut dire qu'on a oublié de sélectionner un choix
+    //Vérifie si on a bien séléctionner un choix
     if (affichage) {
-        let choix = affichage.value;
-    } else {
+        choix = affichage.value;
+    } 
+    else{
         alert("Aucun choix de partie n'a été sélectionné.");
     }
-    */
     
-    fetch(`yams.iutrs.unistra.fr:3000/api/games/$(nomFichier)`)
+    if(choix == 'tout'){
+       affichageGlobal();                //Appel de la fonction qui va afficher toute la partie
+    }
+    
+    if(choix == 'tour'){
+        affichageTourParTour();          //Appel de la fonction qui va afficher tour par tour
+    }
+}
+
+function afficherParametres(){
+    fetch("http://yams.iutrs.unistra.fr:3000/api/games/" + nomFichier + "/parameters")
         .then(response => response.json())
         .then(data => {
-            if(choix === 'tout'){
-                const partieEntiere = document.getElementById('partieComplete');
-                codeHTML = `
-                `;
-                partieEntiere.innerHTML = codeHTML;
-            }
-            else{
-                const tour = document.getElementById('partieReduite');
-                codeHTML = `
-                `;
-                tour.innerHTML = codeHTML;
-            }
+            codeHTML = `
+                <h2>Partie ${nomFichier}</h2>
+                
+                <h3>Paramètres de la partie</h3>
+                <p>Code_Partie : ${data.code}</p>
+                <p>Date_Partie : ${data.date}</p><br>
+            `;
+                contenantParametres.innerHTML = codeHTML;
         })
         .catch(error => console.error('Erreur de chargement du fichier', error));
-});
+}
+
+function afficherJoueurs(){
+    fetch("http://yams.iutrs.unistra.fr:3000/api/games/" + nomFichier + "/players")
+        .then(response => response.json())
+        .then(data =>{
+            codeHTML = `
+                <h3>Les joueurs</h3>
+                <p>ID_Joueur : ${data[0].id}</p>
+                <p>Pseudo_Joueur : ${data[0].pseudo}</p>
+                <br>
+                
+                <p>ID_Joueur : ${data[1].id}</p>
+                <p>Pseudo_Joueur : ${data[1].pseudo}</p>
+                <br>
+            `;
+            contenantJoueurs.innerHTML += codeHTML;
+        })
+        .catch(error => console.error('Erreur de chargement du fichier', error));
+}
+
+function afficherResultatsFinaux(){
+    fetch("http://yams.iutrs.unistra.fr:3000/api/games/" + nomFichier + "/final-result")
+        .then(response => response.json())
+        .then(data =>{
+            codeHTML = `
+                <h3>Scores finaux</h3>
+                <p>ID_Joueur : ${data[0].id_player}</p>
+                <p>Bonus_Joueur : ${data[0].bonus}</p>
+                <p>Score_Joueur : ${data[0].score}</p>
+                <br>
+                
+                <p>ID_Joueur : ${data[1].id_player}</p>
+                <p>Bonus_Joueur : ${data[1].bonus}</p>
+                <p>Score_Joueur : ${data[1].score}</p>
+            `;
+            contenantScoreFinal.innerHTML = codeHTML;
+        })
+        .catch(error => console.error('Erreur de chargement du fichier', error));
+}
+
+function affichageGlobal(){    
+    //Commencer par les paramètres
+    afficherParametres();
+        
+    //Les joueurs
+    afficherJoueurs();
     
+    //Les tours
+    
+    
+    // Fonction asynchrone pour gérer l'asynchronicité des fetch()
+    async function fetchRounds() {
+        for(let i=1; i<14; i++){
+            try{
+                const response = await fetch("http://yams.iutrs.unistra.fr:3000/api/games/" + nomFichier + "/rounds/" + i)
+                const data = await response.json();
+                
+                let codeHTML = `
+                    <h4> Tour ${data.id} </h4>
+                    <h5> Joueur 1 </h5>
+                    <h6> Dés </h6>
+                `;
+                
+                for(let j=0; j<5; j++){
+                    codeHTML += `<p>${data.results[0].dice[j]}</p>`;
+                }
+
+                codeHTML += `
+                    <p>Challenge choisi : ${data.results[0].challenge}</p>
+                    <p>Score du tour : ${data.results[0].score}</p>
+                `;
+
+                
+                // Joueur 2
+                codeHTML += `
+                    <h5> Joueur 2 </h5>
+                    <h6> Dés </h6>
+                `;
+                
+                for(let j=0; j<5; j++){
+                    codeHTML += `<p>${data.results[1].dice[j]}</p>`;
+                }
+
+                codeHTML += `
+                    <p>Challenge choisi : ${data.results[1].challenge}</p>
+                    <p>Score du tour : ${data.results[1].score}</p>
+                `;
+
+                contenantTours.innerHTML += codeHTML;
+                
+            } 
+            catch (error) {
+                console.error('Erreur de chargement du fichier', error);
+            }
+        }
+    }
+    
+    // Appel de la fonction asynchrone
+    codeHTML = `
+        <h3>Les tours</h3>
+    `;
+    contenantTours.innerHTML += codeHTML;
+    fetchRounds();
+    
+    //Résultats finaux
+    afficherResultatsFinaux();
+}   
+
+
+function afficheTour(tour){
+    fetch("http://yams.iutrs.unistra.fr:3000/api/games/" + nomFichier + "/tour/" + tour)
+        .then(response => response.json())
+        .then(data => {
+            let codeHTML = `
+                <h4> Tour ${data.id} </h4>
+                <h5> Joueur 1 </h5>
+                <h6> Dés </h6>
+            `;
+            
+            for(let j=0; j<5; j++){
+                codeHTML += `<p>${data.results[0].dice[j]}</p>`;
+            }
+
+            codeHTML += `
+                <p>Challenge choisi : ${data.results[0].challenge}</p>
+                <p>Score du tour : ${data.results[0].score}</p>
+            `;
+
+            
+            // Joueur 2
+            codeHTML += `
+                <h5> Joueur 2 </h5>
+                <h6> Dés </h6>
+            `;
+            
+            for(let j=0; j<5; j++){
+                codeHTML += `<p>${data.results[1].dice[j]}</p>`;
+            }
+
+            codeHTML += `
+                <p>Challenge choisi : ${data.results[1].challenge}</p>
+                <p>Score du tour : ${data.results[1].score}</p>
+            `;
+
+            contenantTours.innerHTML = codeHTML;
+        })
+        .catch(error => console.error('Erreur de chargement du fichier', error));
+}
+
+
+function affichageTourParTour(){
+    //Rajouter les boutons (changer de tour)
+    codeHTML = `
+        <input type='submit' value='TourPrecedent'>
+        <input type='submit' value='TourSuivant'>
+    `;
+    contenantBouton.innerHTML = codeHTML;
+    
+    
+    //Afficher les tours
+    let i = 1;
+    afficheTour(i);
 
     
-//Je m'amuse avec le JS (cliquez sur l'image !)
+    //Afficher les résultats finaux
+    afficherResultatsFinaux();
+}
     
+    
+    
+    
+    
+    
+    
+
 let image = document.querySelector("img");
 
 image.addEventListener("click", (event) => {
